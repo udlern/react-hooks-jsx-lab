36616388ae60fb1b8ff040663ebfd26398745349
Noa Udler <udlernoa4@gmail.com>
2021-11-24 14:00:07 -0800

    commit tree 2501882fae8f8ee3ffe5105fed6d45f7f209e327

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,15 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  const h1Style = {
+    color: "firebrick"
+  }
+
+  return (
+  <div id="home">
+    <h1 style={h1Style}>{ name } is a Web Developer from { city }</h1>
+  </div> 
+    )
 }
 
 export default Home;
